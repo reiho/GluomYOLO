df506c71e47c404c77c26cc80c2ca446eaa2bfee
Anastasiia Taraskina <35592646+reiho@users.noreply.github.com>
2023-04-20 22:28:15 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, flash, request, redirect, url_for,render_template
+from flask import Flask, request, redirect, url_for, jsonify
 from werkzeug.utils import secure_filename
 import pandas as pd
 import shutil
@@ -9,51 +9,50 @@ app = Flask(__name__)
 DOWNLOAD_DIRECTORY = "./photos"
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 app.config['UPLOAD_FOLDER'] = DOWNLOAD_DIRECTORY
+app.secret_key = "ksljgkldgjlk"
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/predict/<filename>')
 def predict(filename):
     nutr = pd.read_excel('products.xlsx', header=0).set_index('Unnamed: 0')
     nutr.columns = ['name', 'kcal', 'protein', 'fat', 'carbohydrate', 'label']
     labels = pd.read_excel('products.xlsx', header=None, sheet_name='Лист1').to_dict()[0]
     os.system("python yolov5/detect.py --weights epoch62.pt --conf 0.05 --source ./photos/photo.jpg --save-txt --save-conf --name food")
-    shutil.move('./yolov5/runs/detect/food/photo.jpg', 'static/photo.jpg')
+    #shutil.move('./yolov5/runs/detect/food/photo.jpg', 'static/photo.jpg')
     try:
         output = pd.read_csv('./yolov5/runs/detect/food/labels/photo.txt', sep=' ', header=None)
     except:
-        return render_template('index.html')
+        return None
     output.columns = ['label', 'coord1', 'coord2', 'coord3', 'coord4', 'conf']
+    output.sort_values('conf', ascending=False)
     output.label = output.label.replace(labels)
-    df=nutr[nutr.label.isin(output.label)]
-    df.to_html('static/table.html')
+    df=nutr[nutr.label.isin(output.label)].set_index('name')#.drop('Unnamed: 0', axis=1)
+    results=df.T.to_json(force_ascii=False)#.encode('utf-8')
     shutil.rmtree("./yolov5/runs/detect/food")
+    return results
 
-@app.route('/results',methods = ['GET','POST'])
-def show_results():
-    return render_template('results.html')
 
-@app.route('/',methods = ['GET','POST'])
+@app.route('/nutrition',methods = ['POST'])
 def getphoto():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+            return('No file part')
+
         file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+            return('No selected file')
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'photo.jpg'))
-            predict(filename)
-            return redirect('/results')
-            #redirect(url_for('download_file', name=filename))
-    return render_template('index.html')
+            return redirect(url_for('predict', filename=filename))
+
 
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0')
